a4cc291537932dbf1a73a0eb8813cc4824ee2d0b
ganesh.chauhan@amniltech.com <ganesh.chauhan@amniltech.com>
2018-10-10 11:27:35 +0545

    end of section 5

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -21,6 +21,6 @@ export class AuthService {
       );
   }
   register(model: any) {
-return this.http.post(this.baseUrl + 'register', model)
+    return this.http.post(this.baseUrl + 'register', model);
   }
 }
